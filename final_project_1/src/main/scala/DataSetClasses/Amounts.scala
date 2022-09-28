98594b2688fc166290ae3c0216e22b71e37f4432
TKurcabaG <106528867+TKurcabaG@users.noreply.github.com>
2022-09-28 15:25:35 +0200

    Delete final_project_1 directory

--- final_project_1/src/main/scala/DataSetClasses/Amounts.scala
+++
@@ -1,9 +0,0 @@
-package DataSetClasses
-import org.apache.spark.sql.{Encoder, Encoders}
-
-case class Amounts(positionId:Long, amount:BigDecimal, eventTime:Long)
-
-
-object Amounts {
-  implicit val amountEncoder: Encoder[Amounts] = Encoders.product[Amounts]
-}
--- final_project_1/src/main/scala/SparkSessionProvider.scala
+++
@@ -1,7 +0,0 @@
-import org.apache.spark.sql.SparkSession
-
-trait SparkSessionProvider {
-  private val spark = SparkSession.builder().master("local[4]").appName("FinalProject").getOrCreate()
-
-  def getSpark : SparkSession  = spark
-}
