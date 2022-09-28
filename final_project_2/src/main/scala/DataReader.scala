13e691ec5c8a3c2fd110b000c25c5312cbdea310
TKurcabaG <106528867+TKurcabaG@users.noreply.github.com>
2022-09-28 15:27:15 +0200

    Delete final_project_2 directory

--- final_project_2/src/main/scala/DataReader.scala
+++
@@ -1,8 +0,0 @@
-
-import org.apache.spark.sql.DataFrame
-
-object DataReader extends SparkSessionProvider {
-  def readAvro(path: String) : DataFrame = {
-    getSpark.read.format("avro").load("data/"+path)
-  }
-}
--- final_project_2/src/main/scala/SparkSessionProvider.scala
+++
@@ -1,6 +0,0 @@
-import org.apache.spark.sql.SparkSession
-
-trait SparkSessionProvider {
-  private val spark = SparkSession.builder().master("local[4]").appName("final_project_2").getOrCreate()
-  def getSpark : SparkSession = spark
-}
